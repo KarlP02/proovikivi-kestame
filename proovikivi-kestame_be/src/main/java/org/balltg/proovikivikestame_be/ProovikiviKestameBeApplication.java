dc8cf3e109c6f6785dd9584edf34eb01771aa312
Karl Piiber <90192557+KarlP02@users.noreply.github.com>
2024-03-12 19:35:03 +0200

    backend

---
+++ proovikivi-kestame_be/src/main/java/org/balltg/proovikivikestame_be/ProovikiviKestameBeApplication.java
@@ -0,0 +1,13 @@
+package org.balltg.proovikivikestame_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProovikiviKestameBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProovikiviKestameBeApplication.class, args);
+    }
+
+}
---
+++ proovikivi-kestame_be/src/test/java/org/balltg/proovikivikestame_be/ProovikiviKestameBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.balltg.proovikivikestame_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProovikiviKestameBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
